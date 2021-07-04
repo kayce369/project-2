8c41b427591bdc2b2f110d619d0fe024410eca4d
Kayce Georgi <georgi.kayce@ufl.edu>
2021-07-04 14:20:30 -0400

    got splides all working

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 //Splide
 
-new Splide( '.splide', {
-  type: 'loop',
-  perPage: 3,
-  perMove: 1,
-} ).mount();
+var elms = document.getElementsByClassName( 'splide' );
+for ( var i = 0, len = elms.length; i < len; i++ ) {
+	new Splide( elms[ i ], {
+    type: 'loop',
+    perPage: 3,
+    perMove: 1,
+    cover  : true,	height : '15rem'
+  } ).mount();
+}
