5b7b0bbba23664412408081fabc09e846a0d567c
Kayce Georgi <georgi.kayce@ufl.edu>
2021-07-04 14:57:37 -0400

    styled splides

--- js/main.js
+++ js/main.js
@@ -3,9 +3,20 @@
 var elms = document.getElementsByClassName( 'splide' );
 for ( var i = 0, len = elms.length; i < len; i++ ) {
 	new Splide( elms[ i ], {
-    type: 'loop',
+    type:'loop',
     perPage: 3,
     perMove: 1,
-    cover  : true,	height : '15rem'
+    cover  : true,
+    height : '15em',
+    gap: '2em',
+    breakpoints: {
+      700: {
+        perPage: 2
+      },
+      500: {
+        perPage: 1,
+        height : '15em'
+      }
+    }
   } ).mount();
 }
